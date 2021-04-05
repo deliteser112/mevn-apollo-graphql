dd0cee49adfc4fc92492a6ec780dad3221e0a7b2
Super-Web112 <74626440+Super-Web112@users.noreply.github.com>
2021-04-04 20:11:32 -0700

    csv import initatially

--- client/src/components/Posts/AddPost.vue
+++ client/src/components/Posts/AddPost.vue
@@ -10,30 +10,48 @@
 
     <!-- Add Post Form -->
     <v-layout row wrap>
-      <v-flex xs12 sm6 offset-sm3>
-
-        <!-- <post-form :userId="user._id" :parent-name="$options.name"></post-form> -->
-        <!-- <section class="py-5">
-          <div class="row mt-5">
-              <div class="col-8 offset-2">
-                  <h4 class="mb-4">Result:</h4>
-                  <vue-csv-import
-                      v-slot="{file}"
-                      v-model="csv"
-                      :fields="{name: {required: false, label: 'Name'}, age: {required: true, label: 'Age'}}"
-                  >
-                      <vue-csv-errors></vue-csv-errors>
-                      <vue-csv-toggle-headers></vue-csv-toggle-headers>
-                      <vue-csv-input></vue-csv-input>
-                      <vue-csv-map :auto-match="false"></vue-csv-map>
-                      <vue-csv-submit url="/"></vue-csv-submit>
-                  </vue-csv-import>
-                  <pre class="mt-15" v-if="csv"><code>{{ csv }}</code></pre>
+      <!-- <v-flex xs12 sm6 offset-sm3> -->
+          <div class="container">
+            <div class="panel panel-sm">
+              <div class="panel-heading"> 
+                <h4>CSV Import</h4>
               </div>
+              <div class="panel-body">
+                <div class="form-group">
+                  <label for="csv_file" class="control-label col-sm-3 text-right">CSV file to import</label>
+                  <div class="col-sm-9">
+                    <input type="file" id="csv_file" name="csv_file" class="form-control" @change="loadCSV($event)">
+                  </div>
+                </div>
+                
+                <div class="col-sm-offset-3 col-sm-9">
+                  <a href="#" class="btn btn-primary">Parse CSV</a>
+                </div>
+                <table v-if="parse_csv">
+                  <thead>
+                    <tr>
+                      <th v-for="key in parse_header"
+                          @click="sortBy(key)"
+                          :class="{ active: sortKey == key }">
+                        {{ key | capitalize }}
+                        <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'">
+                        </span>
+                      </th>
+                    </tr>
+                  </thead> 
+                  <tr v-for="csv in parse_csv">
+                    <td v-for="key in parse_header">
+                      <input type="text" :value="csv[key]" class="input-cell"/>
+                    </td>
+                  </tr>
+                  
+                </table>
+              </div>
+            </div>
+            
           </div>
-      </section> -->
-
-      </v-flex>
+        <!-- <post-form :userId="user._id" :parent-name="$options.name"></post-form> -->
+      <!-- </v-flex> -->
     </v-layout>
 
   </v-container>
@@ -50,7 +68,19 @@
     data() {
       return {
         headline: 'Import CSV',
+        channel_name: '',
+        channel_fields: [],
+        channel_entries: [],
+        parse_header: [],
+        parse_csv: [],
+        sortOrders:{},
+        sortKey: ''
       };
+    },
+    filters: {
+      capitalize: function (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+      }
     },
     computed: {
       ...mapState(['user', 'error', 'loading'])
@@ -66,10 +96,102 @@
     },
     methods: {
       addPost(post) {
-          this.$store.dispatch('addPost', post);
-          this.$router.push("/");
+        this.$store.dispatch('addPost', post);
+        this.$router.push("/");
+      },
+      sortBy: function (key) {
+        var vm = this
+        vm.sortKey = key
+        vm.sortOrders[key] = vm.sortOrders[key] * -1
+      },
+      csvJSON(csv){
+        var vm = this
+        var lines = csv.split("\n")
+        console.log(lines)
+        var result = []
+        var headers = lines[0].split(",")
+        vm.parse_header = lines[0].split(",") 
+        lines[0].split(",").forEach(function (key) {
+          vm.sortOrders[key] = 1
+        })
+        
+        lines.map(function(line, indexLine){
+          if (indexLine < 1) return // Jump header line
+          
+          var obj = {}
+          var currentline = line.split(",")
+          
+          headers.map(function(header, indexHeader){
+            obj[header] = currentline[indexHeader]
+          })
+          
+          result.push(obj)
+        })
+        
+        result.pop() // remove the last item because undefined values
+        
+        return result // JavaScript object
+      },
+      loadCSV(e) {
+        var vm = this
+        if (window.FileReader) {
+          var reader = new FileReader();
+          reader.readAsText(e.target.files[0]);
+          // Handle errors load
+          reader.onload = function(event) {
+            var csv = event.target.result;
+            vm.parse_csv = vm.csvJSON(csv)
+            
+          };
+          reader.onerror = function(evt) {
+            if(evt.target.error.name == "NotReadableError") {
+              alert("Canno't read file !");
+            }
+          };
+        } else {
+          alert('FileReader are not supported in this browser.');
         }
-
+      }
     }
   };
 </script>
+
+<style>
+  .panel {
+    border: 2px solid #dfdfdf;
+    box-shadow: rgba(0, 0, 0, 0.15) 0 1px 0 0;
+    margin: 10px;
+  } 
+  .panel.panel-sm {
+    max-width: 700px;
+    margin: 10px auto;
+  }
+  .panel-heading {
+    border-bottom: 2px solid #dfdfdf;
+  }
+  .panel-heading h1, .panel-heading h2, .panel-heading h3, .panel-heading h4, .panel-heading h5, .panel-heading h6 {
+    margin: 0;
+    padding: 0;
+  }
+  .panel-body .checkbox-inline {
+    padding: 15px 20px;
+  }
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  td, th {
+    border: 1px solid #b7aeae;
+    text-align: left;
+    padding: 8px;
+  }
+
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
+  .input-cell{
+    width:100%;
+  }
+</style>
