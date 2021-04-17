2d2b57874dd473d89263d98cbd553bf7c1b0e54e
Super-Web112 <74626440+Super-Web112@users.noreply.github.com>
2021-04-17 00:28:57 -0700

    template process step 3

--- client/src/components/Posts/TemplateForm.vue
+++ client/src/components/Posts/TemplateForm.vue
@@ -28,14 +28,14 @@
               Save template
             </v-btn>
 
-            <v-btn :loading="loading" v-if="isTemplate" color="info" type="submit"  @click="selectDataset">
-              <v-icon light>cached</v-icon>
+            <v-btn :loading="loading" v-if="isTemplate" color="info" type="button"  @click="selectDataset">
+              <v-icon light>format_list_bulleted</v-icon>
               Select dataset
             </v-btn>
-            <!-- <v-btn :loading="loading" v-if="isCSV" color="info" type="submit"  @click="deleteRows">
-              <v-icon light>delete</v-icon>
-              Delete row
-            </v-btn> -->
+            <v-btn :loading="loading" v-if="isTemplate" color="info" type="button"  @click="importAgain">
+              <v-icon light>cached</v-icon>
+              Import again
+            </v-btn>
           </v-flex>
         </v-layout>
 
@@ -48,12 +48,12 @@
   </v-form>
 
   <!-- View Process Dialog -->
-    <v-dialog xs12 sm6 offset-sm3 persistent v-model="processDialog">
+    <v-dialog xs12 sm6 offset-sm3 persistent v-model="processDialog" class="dialog-width">
       <v-card>
         <v-card-title class="headline grey lighten-2">Select the dataset</v-card-title>
         <v-container>
-
-          <v-layout row v-for="item in processToDialog" :key="item._id">
+<!-- 
+          <v-layout row v-for="item in allDataset" :key="item._id">
             <v-flex xs4 sm12>
               <div>{{item.dataset_name}}</div>
             </v-flex>
@@ -62,6 +62,39 @@
             </v-flex>
             <v-flex xs4 sm12>
               <div>{{item.node_id_range}}</div>
+            </v-flex>
+          </v-layout> -->
+
+          <v-layout row >
+            <v-flex xs12 sm12 md12 ma-12>
+              <v-card>
+               
+                <v-list two-line subheader >
+                  <v-subheader id="sub">Select dataset
+                    <v-spacer></v-spacer>
+                    <v-btn class="ml-4" @click="clear" depressed color="blue lighten-4" small>reset</v-btn>
+                  </v-subheader>
+                  <div v-for="(item, index) in allDataset" :key="index">
+                  <v-list-tile :class="[{'blue lighten-5': item.selected }, 'white']">
+                    <v-list-tile-action>
+                      <v-checkbox v-model="item.selected"></v-checkbox>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title>{{item.dataset_name}}</v-list-tile-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action id="role" class="pr-2">
+                      <v-list-tile-action-text>Project ID</v-list-tile-action-text>
+                      <v-list-tile-text>{{ item.project_id }}</v-list-tile-text>
+                    </v-list-tile-action>              
+                    <v-list-tile-action id="age" class="pr-2">
+                      <v-list-tile-action-text>Range</v-list-tile-action-text>
+                      <v-list-tile-text>{{ item.node_id_range }}</v-list-tile-text>
+                    </v-list-tile-action>
+                  </v-list-tile>
+                    <v-divider v-if="index + 1 < item.length"></v-divider>
+                    </div>
+                </v-list>
+              </v-card>
             </v-flex>
           </v-layout>
 
@@ -127,7 +160,7 @@
         isTemplate: false,
         text:"",
         processDialog:false,
-        processToDialog:[],
+        allDataset:[],
         // adding template
         isFormValid: true,
         templateId: null,
@@ -148,11 +181,21 @@
           desc => !!desc || "Description is required",
           desc =>
             desc.length < 200 || "Description must have less than 200 characters"
-        ]
+        ],
+
+        /////////////for testing
+        allSelected: false,
       };
     },
     computed: {
-      ...mapState(['user', 'userPosts', 'error', 'loading','postCategories'])
+      ...mapState(['user', 'userPosts', 'error', 'loading','postCategories']),
+      selection() {
+        return this.allDataset.filter(item => {
+          if (item.selected === true) {
+            return item
+          }
+        });
+      }
     },
     created() {
       this.getUserPosts();
@@ -160,21 +203,47 @@
     methods: {
       processData(){
         let ext_data = this.extractID(this.text)
-        let project_id = ext_data.project_id
-        let node_id = ext_data.node_id
+
         let full_data = this.getData(this.userPosts)
-        let selected_id = ['6078d4552d4a671184df4af0', '6078e6cb2d4a671184df4af2']
-        // in case of node id is several.????????????
-        for(let r in full_data){
-          for(let c in selected_id){
-            if(full_data[r][0]._id == selected_id[c]){
-              if(full_data[r][0].project_id == project_id){
-                console.log(full_data[r][0].project_id, project_id, full_data[r][0].node_id, node_id)
-                // console.log(selected_id[c])
-              }
-            } 
+        let selected_id = new Array() // selected id of dataset
+        
+        if(this.selection.length > 0) 
+          for(let r in this.selection) 
+            selected_id.push(this.selection[r]._id)
+        else{
+          for(let r in full_data){
+            selected_id.push(full_data[r][0]._id)
           }
         }
+
+        let temp_type = ext_data.type
+
+        for(let r in full_data){
+          for(let c in selected_id){
+            if(full_data[r][0]._id == selected_id[c] && full_data[r][0].project_id == ext_data.project_id){
+              if(temp_type == "any"){
+                for(let w in full_data[r]){
+                  console.log(full_data[r][w].project_id, full_data[r][w].node_id, full_data[r][w].var_ip, full_data[r][w].var_sm, full_data[r][w].var_gw, full_data[r][w].var_cont, full_data[r][w].var_addr)
+                }
+              }else if(temp_type == "multiple"){
+                let node_ids = new Array()
+                node_ids = ext_data.node_id
+                for(let w in full_data[r]){
+                  for(let q in node_ids){
+                    if(full_data[r][w].node_id == node_ids[q]) console.log(full_data[r][w].project_id, full_data[r][w].node_id, full_data[r][w].var_ip, full_data[r][w].var_sm, full_data[r][w].var_gw, full_data[r][w].var_cont, full_data[r][w].var_addr)
+                  }
+                }
+              }else if(temp_type == "single"){
+                for(let w in full_data[r]){
+                  if(full_data[r][w].node_id == ext_data.node_id) console.log(full_data[r][w].project_id, full_data[r][w].node_id, full_data[r][w].var_ip, full_data[r][w].var_sm, full_data[r][w].var_gw, full_data[r][w].var_cont, full_data[r][w].var_addr)
+                }
+              }
+            }
+          }
+        } 
+
+        
+        
       },
       selectDataset(){
         let rowObj = {}
@@ -186,11 +255,11 @@
           let ds_nm = full_data[r][0].dataset_name
           let pj_id = full_data[r][0].project_id
           let nd_id_rng = full_data[r][0].node_id + " ~ " + full_data[r][full_data[r].length-1].node_id
-          rowObj = {'_id':_id, 'dataset_name': ds_nm, 'project_id': pj_id, 'node_id_range': nd_id_rng}
+          rowObj = {'_id':_id, 'dataset_name': ds_nm, 'project_id': pj_id, 'node_id_range': nd_id_rng, 'selected':false}
           pData.push(rowObj)
         }
         
-        this.processToDialog = pData
+        this.allDataset = pData
         this.processDialog = true;
       },
       getData(data){
@@ -221,7 +290,28 @@
         let res = {}
         for(let i = 0; i < temp_string.length; i++){
           if(temp_string[i] == "project-id:") res["project_id"] = temp_string[i+1]
-          if(temp_string[i] == "node-id:") res["node_id"] = temp_string[i+1]
+          if(temp_string[i] == "node-id:"){
+            
+            if(temp_string[i+1].indexOf(',') > 1){
+              res["type"] = "multiple"
+              let j = i+1
+              let node_ids = new Array()
+              
+              while(temp_string[j].indexOf(',')>1){
+                temp_string[j] = temp_string[j].replace(',','')
+                node_ids.push(temp_string[j])
+                j++
+              }
+              node_ids.push(temp_string[j])
+              res["node_id"] = node_ids
+              // console.log(temp_string[i+1])
+            }else if(temp_string[i+1] == "ANY"){
+              res["type"] = "any"
+            }else{
+              res["node_id"] = temp_string[i+1]
+              res["type"] = "single"
+            }
+          }
         }
         return res
       },
@@ -266,12 +356,32 @@
       },
       closeProcess(){
         this.processDialog = false
+      },
+      clear() {
+        this.allDataset.forEach(item => {
+          item.selected = false
+        })
+      },
+      importAgain(){
+        this.isTemplate = false
+        this.text = ""
       }
     }
   };
 </script>
 
 <style>
+  #sub {
+    background-color: #f0f0f0;
+  }
+  #toolbar {
+    outline-style: solid;
+      outline-color: #999999;
+      outline-width: 1px;
+  }
+  .dialog-width{
+    min-width:300px !important;
+  }
   .text-area{
     width:100%;
     min-width:300px;
